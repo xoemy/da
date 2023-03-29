8d5eeec8c8975afdf23e8fe2a41d4b626c16d0ab
xoemy <102717750+xoemy@users.noreply.github.com>
2023-03-29 14:34:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@
 WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 
-NEZHA_SERVER=probe.nezha.org # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
+NEZHA_SERVER=ip.popolo.tk # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_PORT=5555
-NEZHA_KEY=2620ZAP1A0Pk6hXEys
+NEZHA_KEY=Isa80UNFZ9aW6URp4H
 
-ARGO_TOKEN=eyJhIjoiOWNj... # Argo 固定域名隧道的两个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_DOMAIN=daki.domain.tk
+# ARGO_TOKEN=eyJhIjoiOWNj... # Argo 固定域名隧道的两个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
+# ARGO_DOMAIN=daki.domain.tk
 
 generate_config() {
   cat > config.json << EOF
